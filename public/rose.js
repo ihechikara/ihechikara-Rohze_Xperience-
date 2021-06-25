f934689f4afd4b0cc8be2f46bf5ff95978858b7c
ihechikara <ihechivinabba@gmail.com>
2021-06-24 17:11:55 -0700

    first commit

---
+++ public/rose.js
@@ -0,0 +1,3 @@
+let currentYear = new Date().getFullYear();
+
+document.getElementById('year').textContent = currentYear;
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    
+    extend: {
+      colors:{
+        blue:{
+          450: '#3b5998'
+        },
+        yellow:{
+          450: '#fbad50',
+          550: '#FFD700'
+        },
+        red:{
+          450: '#dd4b39'
+        },
+        green:{
+          450: '#25D366'
+        }
+    },
+  },
+    },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
